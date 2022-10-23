bfff8281353ec2ea4e18e68f431e8e47a005f3ff
Gustavo Vargas Miranda <gvmgust@gmail.com>
2022-10-23 01:05:11 -0400

    Cambiando useLocalStorage a reducer
    agregando ChangeAlert

--- src/App/index.js
+++ src/App/index.js
@@ -1,38 +1,40 @@
 import React from "react";
-import "./App.css";
 import { useTodos } from "./useTodos";
-import { TodoList } from "../TodoList";
-import { CreateTodoButtom } from "../CreateTodoButtom";
 import { TodoHeader } from "../TodoHeader";
 import { TodoCounter } from "../TodoCounter";
-import { TodoItem } from "../TodoItem";
 import { TodoSearch } from "../TodoSearch";
-import { TodoForm } from "../TodoForm";
-import { Modal } from "../Modal";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
 import { TodosError } from "../TodosError";
 import { TodosLoading } from "../TodosLoading";
 import { EmptyTodos } from "../EmptyTodos";
+import { TodoForm } from "../TodoForm";
+import { CreateTodoButtom } from "../CreateTodoButtom";
 import { TodoConfirmation } from "../TodoConfirmation";
+import { Modal } from "../Modal";
+import { ChangeAlert } from "../ChangeAlert";
+import "./App.css";
 
 function App() {
   const {
+    error,
+    loading,
+    searchValue,
     todos,
     totalTodos,
     completedTodos,
-    searchValue,
     openModal,
     openConfirmation,
-    error,
-    loading,
     todoCountSearch,
     todoToDelete,
     completTodo,
     createTodo,
-    deleteTodo,
     setSearchValue,
     setTodoToDelete,
     setOpenConfirmation,
+    deleteTodo,
     setOpenModal,
+    sincronizeTodos,
   } = useTodos();
   return (
     <div id="App" >
@@ -52,20 +54,23 @@ function App() {
         todoCountSearch={todoCountSearch}
         searchValue={searchValue}
         todos={todos}
-        onError={error => <TodosError error={error}/>}
+        onError={error => <TodosError error={error} />}
         onLoading={() => <TodosLoading />}
-        onEmptyTodos={() => <EmptyTodos message={"No se han encontrado Tareas, Agregar Nueva Tarea con el botón (+)"}/>}
-        onEmptyEmptySearchResults={() => <EmptyTodos message={`no se han encontrado resultados para "${searchValue.toUpperCase()}"`}/>}
+        onEmptyTodos={() => <EmptyTodos message={"No se han encontrado Tareas, Agregar Nueva Tarea con el botón (+)"} />}
+        onEmptyEmptySearchResults={() => <EmptyTodos message={`no se han encontrado resultados para "${searchValue.toUpperCase()}"`} />}
       //render=
       >
         {todo => (
-          <TodoItem
-            key={todo.text}
-            todo={todo}
-            completTodo={completTodo}
-            setTodoToDelete={setTodoToDelete}
-            setOpenConfirmation={setOpenConfirmation}
-          />)}
+          !loading && (
+            <TodoItem
+              key={todo.text}
+              todo={todo}
+              completTodo={completTodo}
+              setTodoToDelete={setTodoToDelete}
+              setOpenConfirmation={setOpenConfirmation}
+            />
+          )
+        )}
       </TodoList>
 
       {(openModal || openConfirmation) && (
@@ -83,13 +88,19 @@ function App() {
         </Modal>
       )}
 
-      <CreateTodoButtom
-        openModal={openModal}
-        setOpenModal={setOpenModal}
-        openConfirmation={openConfirmation}
-        setOpenConfirmation={setOpenConfirmation}
-        setTodoToDelete={setTodoToDelete}
+      <ChangeAlert
+        sincronize={sincronizeTodos}
       />
+      {
+        !loading && (
+          <CreateTodoButtom
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            openConfirmation={openConfirmation}
+            setOpenConfirmation={setOpenConfirmation}
+            setTodoToDelete={setTodoToDelete}
+          />)
+      }
     </div>
   );
 }
